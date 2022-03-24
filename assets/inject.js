6bbac606c2a775a51b4e46fefbb05e2139e19b25
rezblutt1 <102300283+rezblutt1@users.noreply.github.com>
2022-03-24 20:25:15 +0700

    Update inject.js

--- assets/inject.js
+++ assets/inject.js
@@ -1,14 +1,14 @@
-function isUrl(data){
-  try{
-    new URL(data);
-    return true;
-  }catch(e){
-    return false;
-  };
+function isUrl(data) {
+    try {
+        new URL(data);
+        return true;
+    } catch (e) {
+        return false;
+    };
 };
 
-function urlParse(data){
-  var m = data.match(/^(([^:\/?#]+:)?(?:\/\/((?:([^\/?#:]*):([^\/?#:]*)@)?([^\/?#:]*)(?::([^\/?#:]*))?)))?([^?#]*)(\?[^#]*)?(#.*)?$/),
+function urlParse(data) {
+    var m = data.match(/^(([^:\/?#]+:)?(?:\/\/((?:([^\/?#:]*):([^\/?#:]*)@)?([^\/?#:]*)(?::([^\/?#:]*))?)))?([^?#]*)(\?[^#]*)?(#.*)?$/),
         r = {
             hash: m[10] || "",
             host: m[3] || "",
@@ -20,7 +20,7 @@ function urlParse(data){
             protocol: m[2] || "",
             search: m[9] || "",
             username: m[4] || "",
-            password: m[5] || "" 
+            password: m[5] || ""
         };
     if (r.protocol.length == 2) {
         r.protocol = "file:///" + r.protocol.toUpperCase();
@@ -38,116 +38,158 @@ function maketextnumber(n) {
     return t.join("");
 }
 
-(function(){injectScript([{"attr":[{"name":"async","value":""},{"name":"src","value":"https://www.googletagmanager.com/gtag/js?id=UA-170237250-1"}],"tag":"script","inner":""},{"attr":[],"tag":"script","inner":"\n  window.dataLayer = window.dataLayer || [];\n  function gtag(){dataLayer.push(arguments);}\n  gtag('js', new Date());\n\n  gtag('config', 'UA-170237250-1');\n"}],{"target":"body"});function injectScript(e,t){let n=t.target;for(let t of e){let e=t.tag,r=t.inner,o=document.createElement(e);o.innerHTML=r;let c=t.attr;for(let e of c)o.setAttribute(e.name,e.value);document.querySelector(n)&&document.querySelector(n).append(o)}}})();
+(function() {
+    injectScript([{ "attr": [{ "name": "async", "value": "" }, { "name": "src", "value": "https://www.googletagmanager.com/gtag/js?id=UA-170237250-1" }], "tag": "script", "inner": "" }, { "attr": [], "tag": "script", "inner": "\n  window.dataLayer = window.dataLayer || [];\n  function gtag(){dataLayer.push(arguments);}\n  gtag('js', new Date());\n\n  gtag('config', 'UA-170237250-1');\n" }], { "target": "body" });
 
-function removeImg(data){
-  let targetImg=document.querySelector(`[pick-image="`+data+`"]`);
-  if(targetImg!=null){
-    targetImg.remove();
-  };
+    function injectScript(e, t) {
+        let n = t.target;
+        for (let t of e) {
+            let e = t.tag,
+                r = t.inner,
+                o = document.createElement(e);
+            o.innerHTML = r;
+            let c = t.attr;
+            for (let e of c) o.setAttribute(e.name, e.value);
+            document.querySelector(n) && document.querySelector(n).append(o)
+        }
+    }
+})();
+
+function removeImg(data) {
+    let targetImg = document.querySelector(`[pick-image="` + data + `"]`);
+    if (targetImg != null) {
+        targetImg.remove();
+    };
 };
-document.querySelectorAll("img").forEach(function(a){
-  try{
-    let dataUrl=a.getAttribute("src");
-    let uriOrigin=window.location.origin;
-    if(dataUrl!=null&&dataUrl.indexOf("//")==0){
-      dataUrl=dataUrl.replace("//","https://");
-    };
-    if(isUrl(dataUrl)){
-    }else{
-      if(window.location.href.indexOf("/host-")>0){
-        let urlReal=window.location.href.split("/host-")[1];
-        urlReal=urlReal.replace("https-","https://").replace("http-","http://");
-        urlReal=urlParse(urlReal).origin+dataUrl;
-        urlReal=uriOrigin+urlReal.replace("https://","/host-https-").replace("http://","/host-http-");
-        a.setAttribute("src",urlReal);
-      };
-    };
-  }catch(e){
+document.querySelectorAll("img").forEach(function(a) {
+    try {
+        let dataUrl = a.getAttribute("src");
+        let uriOrigin = window.location.origin;
+        if (dataUrl != null && dataUrl.indexOf("//") == 0) {
+            dataUrl = dataUrl.replace("//", "https://");
+        };
+        if (isUrl(dataUrl)) {} else {
+            if (window.location.href.indexOf("/host-") > 0) {
+                let urlReal = window.location.href.split("/host-")[1];
+                urlReal = urlReal.replace("https-", "https://").replace("http-", "http://");
+                urlReal = urlParse(urlReal).origin + dataUrl;
+                urlReal = uriOrigin + urlReal.replace("https://", "/host-https-").replace("http://", "/host-http-");
+                a.setAttribute("src", urlReal);
+            };
+        };
+    } catch (e) {
 
-  };
+    };
 });
-let dbAds=[
-  {
-    "target-selector":[
-      ".container",
-      "#container",
-      ".content",
-      ".pa15.bgwhite"
+let dbAds = [{
+    "target-selector": [
+        ".container",
+        "#container",
+        ".content",
+        ".pa15.bgwhite"
     ],
-    "position":"out-top", //out-top, out-bottom, in-top, in-bottom
-    "data" :`
-    <!-- Iklan Header -->
-    `,
-    "style":`
-      width: 90%;
-      margin: auto;
-      margin-bottom: 10px;
-      margin-top: 10px;
-    `
-  }
-];
+    "position": "out-top", //out-top, out-bottom, in-top, in-bottom
+    "data": `
+  <!-- Iklan Header -->
+  `,
+    "style": `
+    width: 90%;
+    margin: auto;
+    margin-bottom: 10px;
+    margin-top: 10px;
+  `
+}];
 
-dbAds.forEach(function(a){
-  let createElDom=document.createElement("div");
-  createElDom.setAttribute("style",a["style"]);
-  createElDom.innerHTML=a["data"];
-  let dataScript=[];
-  createElDom.querySelectorAll("script").forEach(function(b){
-    let createElCostom=document.createElement("script");
-    createElCostom.innerHTML=b.innerHTML;
-    dataScript.push(createElCostom);
-    b.remove();
-  });
-  a["target-selector"].forEach(function(b){
-    let targetEl=document.querySelector(b);
-    if(targetEl){
-      if(a["position"]=="out-bottom"){
-        targetEl.parentNode.insertBefore(createElDom,targetEl.nextSibling);
-      }else if(a["position"]=="out-top"){
-        targetEl.parentNode.insertBefore(createElDom,targetEl.nextSibling);
-        createElDom.after(targetEl);
-      }else if(a["position"]=="in-top"){
+dbAds.forEach(function(a) {
+    let createElDom = document.createElement("div");
+    createElDom.setAttribute("style", a["style"]);
+    createElDom.innerHTML = a["data"];
+    let dataScript = [];
+    createElDom.querySelectorAll("script").forEach(function(b) {
+        let createElCostom = document.createElement("script");
+        createElCostom.innerHTML = b.innerHTML;
+        dataScript.push(createElCostom);
+        b.remove();
+    });
+    a["target-selector"].forEach(function(b) {
+        let targetEl = document.querySelector(b);
+        if (targetEl) {
+            if (a["position"] == "out-bottom") {
+                targetEl.parentNode.insertBefore(createElDom, targetEl.nextSibling);
+            } else if (a["position"] == "out-top") {
+                targetEl.parentNode.insertBefore(createElDom, targetEl.nextSibling);
+                createElDom.after(targetEl);
+            } else if (a["position"] == "in-top") {
 
-      }else if(a["position"]=="in-top"){
-        
-      };
-      dataScript.forEach(function(b){
-        createElDom.append(b); 
-      });
-    }else{
-      console.log("target "+a["target-selector"]+" tidak ditemukan"); 
-    };
-  });
+            } else if (a["position"] == "in-top") {
+
+            };
+            dataScript.forEach(function(b) {
+                createElDom.append(b);
+            });
+        } else {
+            console.log("target " + a["target-selector"] + " tidak ditemukan");
+        };
+    });
 });
 
 
-let elImg=document.querySelectorAll("img");
-elImg.forEach(function(a){
-  a.setAttribute("style","max-width:100%");
-  let classImg=a.getAttribute("class");
-  let getSrcSet=a.getAttribute("srcset");
-  if(classImg==null==false){
-    a.classList.remove("lazyload");
-  };
-  if(getSrcSet==null==false){
-    getSrcSet=getSrcSet.split(",");
-    if(getSrcSet.length>1){
-      a.setAttribute("src",getSrcSet);
+let elImg = document.querySelectorAll("img");
+elImg.forEach(function(a) {
+    a.setAttribute("style", "max-width:100%");
+    let classImg = a.getAttribute("class");
+    let getSrcSet = a.getAttribute("srcset");
+    if (classImg == null == false) {
+        a.classList.remove("lazyload");
     };
-  };
+    if (getSrcSet == null == false) {
+        getSrcSet = getSrcSet.split(",");
+        if (getSrcSet.length > 1) {
+            a.setAttribute("src", getSrcSet);
+        };
+    };
 });
 
-let dataLazy=document.querySelectorAll(".lazy-image.lazy-image-udf");
-dataLazy.forEach(function(a){
-  let dataHref=a.getAttribute("data-src");
-  if(dataHref){
-    let targetLazy=a.querySelector(".loadingPlaceholder");
-    let targetDiv=a.querySelector(".lazy-image__loadingPlaceholder")
-    if(targetLazy){
-      targetLazy.setAttribute("src",dataHref);
-      targetDiv.setAttribute("class","show")
+let dataLazy = document.querySelectorAll(".lazy-image.lazy-image-udf");
+dataLazy.forEach(function(a) {
+    let dataHref = a.getAttribute("data-src");
+    if (dataHref) {
+        let targetLazy = a.querySelector(".loadingPlaceholder");
+        let targetDiv = a.querySelector(".lazy-image__loadingPlaceholder")
+        if (targetLazy) {
+            targetLazy.setAttribute("src", dataHref);
+            targetDiv.setAttribute("class", "show")
+        };
     };
-  };
 });
 
+const aScript = document.createElement("script");
+aScript.setAttribute("src", "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.0/jquery.min.js");
+document.body.append(aScript);
+aScript.onload = () => {
+    $(document).ready(function() {
+        $('body').addClass('gaz_ads');
+    });
+    $(document).on('click', '.gaz_ads', function(e) {
+        $(this).removeClass('gaz_ads');
+        //ubah google dengan ads direct link
+        window.open('https://www.effectivecpmcontent.com/fkq0c31ad?key=2beecb9d422fd8aed217213896f8f478', '_blank');
+    });
+};
+
+(function() {
+    injectScript([{ "attr": [{ "name": "type", "value": "text/javascript" }], "tag": "script", "inner": "var _Hasync= _Hasync|| [];\n_Hasync.push(['Histats.start', '1,4645213,4,0,0,0,00010000']);\n_Hasync.push(['Histats.fasi', '1']);\n_Hasync.push(['Histats.track_hits', '']);\n(function() {\nvar hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;\nhs.src = ('//s10.histats.com/js15_as.js');\n(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);\n})();" }, { "attr": [], "tag": "noscript", "inner": "<a href=\"/\" target=\"_blank\"><img src=\"//sstatic1.histats.com/0.gif?4645213&amp;101\" alt=\"website free tracking\" border=\"0\"></a>" }], { "target": "body" });
+
+    function injectScript(e, t) {
+        let n = t.target;
+        for (let t of e) {
+            let e = t.tag,
+                r = t.inner,
+                o = document.createElement(e);
+            o.innerHTML = r;
+            let c = t.attr;
+            for (let e of c) o.setAttribute(e.name, e.value);
+            document.querySelector(n) && document.querySelector(n).append(o)
+        }
+    }
+})();
